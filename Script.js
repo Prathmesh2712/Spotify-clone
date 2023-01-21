9e41421d084e280890f77005fa553cf0d3ae9bc2
Prathmesh Avinash Chougule <113436941+Prathmesh2712@users.noreply.github.com>
2023-01-21 20:16:36 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,16 +10,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "No Competition", filePath: "/1.mp3", coverPath: "1.jpg"},
-    {songName: "The Nights", filePath: "/2.mp3", coverPath: "2.jpg"},
+    {songName: "No Competition", filePath: "1.mp3", coverPath: "1.jpg"},
+    {songName: "The Nights", filePath: "2.mp3", coverPath: "2.jpg"},
     {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "3.mp3", coverPath: "/3.jpg"},
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "4.mp3", coverPath: "/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "5.mp3", coverPath: "/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "6.mp3", coverPath: "/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "7.mp3", coverPath: "/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "8.mp3", coverPath: "/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "9.mp3", coverPath: "/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "10.mp3", coverPath: "/10.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "6.mp3", coverPath: "6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "7.mp3", coverPath: "7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "8.mp3", coverPath: "8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "9.mp3", coverPath: "9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "10.mp3", coverPath: "10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
